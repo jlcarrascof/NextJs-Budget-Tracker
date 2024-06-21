06767c45427d8b81e5bc5695600d25914fce4cdc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 22:01:28 -0400

    refactor: Update auth layout to use a wrapper div for children

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react'
 function layout({children}: {children: React.ReactNode}) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-        layout
+        <div className="mt-12">{children}</div>
     </div>
   )
 }
