a90214edb47cdcc6e5783384291c712e15df9b89
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 23:16:26 -0400

    feat: Add PiggyBank icon to Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,10 @@
+import { PiggyBank } from 'lucide-react'
 import React from 'react'
 
 function Logo() {
   return <a href="/" className="flex items-center gap-2">
-
+    <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]"
+     />
   </a>
 
 }
