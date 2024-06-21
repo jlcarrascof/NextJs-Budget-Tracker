ab2236f69ca99e62e9e6a4d3bc22e70f1f7cac20
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-20 21:59:37 -0400

    feat: Add auth layout component

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+function layout({children}: {children: React.ReactNode}) {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center">
+        layout
+    </div>
+  )
+}
+
+export default layout
