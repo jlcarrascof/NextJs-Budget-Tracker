fa1f417e2bd3071541e28617aac94b04e45b3fe0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-25 19:12:38 -0400

    refactor: Add Navbar component to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Navbar'
 import React, { ReactNode } from 'react'
 
 function layout({children}: {children: React.ReactNode}) {
   return (
     <div className="relative flex h-screen w-full flex-col">
+        <Navbar />
         <div className="w-full">
             {children}
         </div>
