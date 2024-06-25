b70a1dbe940c4ac226d2e1d1b7d1af8d2a3c19b1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-25 19:04:43 -0400

    refactor: Update dashboard layout to include children prop

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,8 +2,12 @@ import React, { ReactNode } from 'react'
 
 function layout({children}: {children: React.ReactNode}) {
   return (
-    <div>layout</div>
-  )
+    <div className="relative flex h-screen w-full flex-col">
+        <div className="w-full">
+            {children}
+        </div>
+    </div>
+  );
 }
 
 export default layout
