a5cf0343b1dd52f4f45a982a7e51e7fe6fdefbfb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-25 19:30:52 -0400

    refactor: Update Navbar component to include desktop version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Logo from './Logo';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+        <DesktopNavbar />
+    </>
+  );
+}
+
+function DesktopNavbar() {
+    return (
+        <div className="hidden border-separate border-b bg-background md:block">
+            <nav className="container flex items-center justify-between px-8">
+                <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+                    <Logo />
+                    <div className="flex h-full">
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar
